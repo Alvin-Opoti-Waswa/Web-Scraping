ee72b57337558dca60e9b6d8ed822f0798b25f69
Opoti Alvin Waswa <150916755+Alvin-Opoti-Waswa@users.noreply.github.com>
2024-04-21 12:16:47 +0200

    Update WebScraping.py

--- WebScraping.py
+++ WebScraping.py
@@ -5,3 +5,8 @@ import pandas as pd
 
 page = urllib.request.urlopen('https://wseiz.pl/uczelnia/wydzial-inzynierii-i-zarzadzania/informatyka-i-stopnia/')
 soup = bs(page)
+
+headings = soup.body.findAll('a')
+headings_names = re.findall('class="toc-backref" href="\w+', str(headings))
+
+print(headings_names)
